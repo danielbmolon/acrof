00a845f6bb909c6637eb4b87af265d3df8356110
Daniel <Daniel@192.168.1.5>
2014-09-08 16:09:06 -0300

    Criada a classe palavras

---
+++ src/com/acrof/model/Palavras.java
@@ -0,0 +1,21 @@
+package com.acrof.model;
+
+public class Palavras {
+	
+	private String palavra;
+	private String categoria;
+	
+	public String getPalavra() {
+		return palavra;
+	}
+	public void setPalavra(String palavra) {
+		this.palavra = palavra;
+	}
+	public String getCategoria() {
+		return categoria;
+	}
+	public void setCategoria(String categoria) {
+		this.categoria = categoria;
+	}
+
+}
